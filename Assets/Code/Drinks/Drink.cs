daf7a6c4935d22a21d0a6c94dcc1575767ed8520
Elayan <blue-violin@hotmail.fr>
2024-07-15 17:56:52 +0200

    Renamed Recipe as Drink

--- Assets/Code/Drinks/Drink.cs
+++ Assets/Code/Drinks/Drink.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Recipe : MonoBehaviour
+public class Drink : MonoBehaviour
 {
     public List<MachineBase> MachinesWhoEdited = new List<MachineBase>();
 }
