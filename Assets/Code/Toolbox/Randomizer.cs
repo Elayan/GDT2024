2d6d583f675ba456674b42af770e08a517108756
Elayan <blue-violin@hotmail.fr>
2024-07-16 18:38:02 +0200

    Homebrewed randomizer to help future debug

---
+++ Assets/Code/Toolbox/Randomizer.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+public class Randomizer
+{
+    // set this to any positive int and you'll have the same rolls each run!
+    public const int DebugSeed = -1;
+
+    static Randomizer()
+    {
+        _instance = new Randomizer();
+
+        if (DebugSeed < 0)
+            _instance._random = new Random();
+        else _instance._random = new Random(DebugSeed);
+    }
+
+    private static Randomizer _instance = null;
+    public static Randomizer Get() => _instance;
+
+    private Random _random;
+
+    public int Next() => _random.Next();
+    public int Next(int max) => _random.Next(max);
+    public int Next(int min, int max) => _random.Next(min, max);
+}
