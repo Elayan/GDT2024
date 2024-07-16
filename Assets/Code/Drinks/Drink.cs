db9f425b3c3c573c416a385bf341248d55fd7864
Elayan <blue-violin@hotmail.fr>
2024-07-16 18:37:38 +0200

    Recipe class + Drink comparison to recipe

--- Assets/Code/Drinks/Drink.cs
+++ Assets/Code/Drinks/Drink.cs
@@ -1,7 +1,74 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Drink : MonoBehaviour
 {
     public List<MachineBase> MachinesWhoEdited = new List<MachineBase>();
+
+    /// <summary>
+    /// Please make this better, my enums are atrocious T-T
+    /// </summary>
+    /// <param name="recipe"></param>
+    /// <returns></returns>
+    public bool IsMatchingRecipe(Recipe recipe, bool debugLog = false)
+    {
+        var colorers = MachinesWhoEdited.OfType<Colorer>().ToArray();
+        var adders = MachinesWhoEdited.OfType<Adder>().ToArray();
+        if (colorers.Length != recipe.Content.Length)
+        {
+            if (debugLog)
+                Debug.LogWarning($"Wrong order: found {colorers.Length} Colorers, expecting {recipe.Content.Length}.");
+            return false;
+        }
+        if (adders.Length != recipe.Extra.Length)
+        {
+            if (debugLog)
+                Debug.LogWarning($"Wrong order: found {adders.Length} Colorers, expecting {recipe.Extra.Length}.");
+            return false;
+        }
+
+        return IsMatchingRecipeColorers(colorers, recipe, debugLog)
+            && IsMatchingRecipeAdders(adders, recipe, debugLog);
+    }
+
+    private bool IsMatchingRecipeColorers(Colorer[] colorers, Recipe recipe, bool debugLog)
+    {
+        var colorersToVerify = colorers.ToList();
+        foreach (var content in recipe.Content)
+        {
+            var match = colorersToVerify.FirstOrDefault(c => c.Content == content);
+            if (match == null)
+            {
+                if (debugLog)
+                    Debug.LogWarning($"Wrong order: didn't find a Colorer with tag {content}.");
+                return false;
+            }
+
+            colorersToVerify.Remove(match);
+        }
+
+        return true;
+    }
+
+    private bool IsMatchingRecipeAdders(Adder[] adders, Recipe recipe, bool debugLog)
+    {
+        var addersToVerify = adders.ToList();
+        foreach (var extra in recipe.Extra)
+        {
+            var match = addersToVerify.FirstOrDefault(c => c.Extra == extra);
+            if (match == null)
+            {
+                if (debugLog)
+                    Debug.LogWarning($"Wrong order: didn't find an Added with tag {extra}.");
+                return false;
+            }
+
+            addersToVerify.Remove(match);
+        }
+
+        return true;
+    }
 }
---
+++ Assets/Code/Drinks/Recipe.cs
@@ -0,0 +1,54 @@
+using System;
+
+public enum RecipeContainer
+{
+    Glass,
+}
+
+public enum RecipeContent
+{
+    Juice,
+    Vodka,
+}
+
+public enum RecipeExtra
+{
+    IceCubes,
+}
+
+public class Recipe
+{
+    private static RecipeContainer[] _allContainers = (RecipeContainer[])Enum.GetValues(typeof(RecipeContainer));
+    private static RecipeContent[] _allContents = (RecipeContent[])Enum.GetValues(typeof(RecipeContent));
+    private static RecipeExtra[] _allExtras = (RecipeExtra[])Enum.GetValues(typeof(RecipeExtra));
+
+    // these should probably be different as we move further in levels
+    public const int MinContent = 1;
+    public const int MaxContent = 3;
+    public const int MinExtra = 0;
+    public const int MaxExtra = 1;
+
+    public RecipeContainer Container { get; private set; }
+    public RecipeContent[] Content { get; private set; }
+    public RecipeExtra[] Extra { get; private set; }
+
+    public static Recipe GenerateRecipe()
+    {
+        var recipe = new Recipe();
+        var rand = Randomizer.Get();
+
+        recipe.Container = _allContainers[rand.Next(_allContainers.Length)];
+
+        var contentCount = rand.Next(MinContent, MaxContent);
+        recipe.Content = new RecipeContent[contentCount];
+        for (var i = 0; i < contentCount; i++)
+            recipe.Content[i] = _allContents[rand.Next(_allContents.Length)];
+
+        var extraCount = rand.Next(MinExtra, MaxExtra);
+        recipe.Extra = new RecipeExtra[extraCount];
+        for (var i = 0; i < extraCount; i++)
+            recipe.Extra[i] = _allExtras[rand.Next(_allExtras.Length)];
+
+        return recipe;
+    }
+}
